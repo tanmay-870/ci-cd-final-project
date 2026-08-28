20044a14119c70dfeab68e1988d9945a2da6f30b
tanmay-870 <2k24.cs1m.2411704@gmail.com>
2026-08-28 02:03:07 -0400

    Automatically add trailing newlines to all python files

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -130,3 +130,4 @@ class CounterTest(TestCase):
         # Gte it to make sure it's really gone
         resp = self.app.get(f"/counters/{name}")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
