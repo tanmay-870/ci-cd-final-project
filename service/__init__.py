a9127617e9fa26e4f9e503b07feb5a916a6c9a1d
tanmay-870 <2k24.cs1m.2411704@gmail.com>
2026-08-28 01:43:59 -0400

    Fix all flake8 linting violations

--- service/__init__.py
+++ service/__init__.py
@@ -1,16 +1,24 @@
 """
-Service Package
+Package: service
+Package for the Counter Service
 """
+import sys
 from flask import Flask
 
+# Create Flask application
 app = Flask(__name__)
 
-# This must be imported after the Flask app is created
-from service import routes               # pylint: disable=wrong-import-position,cyclic-import
-from service.common import log_handlers  # pylint: disable=wrong-import-position
+# Import the routes after the Flask app is created
+from service import routes, models  # noqa: E402, E501
+from service.common import error_handlers, log_handlers  # noqa: E402
 
+# Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
 
 app.logger.info(70 * "*")
-app.logger.info("  S E R V I C E   R U N N I N G  ".center(70, "*"))
+app.logger.info(
+    "  C O U N T E R   S E R V I C E   R U N N I N G  ".center(70, "*")
+)
 app.logger.info(70 * "*")
+
+app.logger.info("Service initialized!")
--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -13,26 +13,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ######################################################################
-
 """
 Module: error_handlers
 """
 from flask import jsonify
 from service import app
-from . import status
+from service.common import status
 
 
-######################################################################
-# Error Handlers
-######################################################################
 @app.errorhandler(status.HTTP_400_BAD_REQUEST)
 def bad_request(error):
-    """Handles bad requests with 400_BAD_REQUEST"""
-    message = str(error)
-    app.logger.warning(message)
+    """Handling Bad Requests"""
+    app.logger.warning(str(error))
     return (
         jsonify(
-            status=status.HTTP_400_BAD_REQUEST, error="Bad Request", message=message
+            status=status.HTTP_400_BAD_REQUEST,
+            error="Bad Request",
+            message=str(error),
         ),
         status.HTTP_400_BAD_REQUEST,
     )
@@ -40,70 +37,41 @@ def bad_request(error):
 
 @app.errorhandler(status.HTTP_404_NOT_FOUND)
 def not_found(error):
-    """Handles resources not found with 404_NOT_FOUND"""
-    message = str(error)
-    app.logger.warning(message)
+    """Handling Not Found errors"""
+    app.logger.warning(str(error))
     return (
-        jsonify(status=status.HTTP_404_NOT_FOUND, error="Not Found", message=message),
+        jsonify(
+            status=status.HTTP_404_NOT_FOUND,
+            error="Not Found",
+            message=str(error),
+        ),
         status.HTTP_404_NOT_FOUND,
     )
 
 
-@app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
-def method_not_supported(error):
-    """Handles unsupported HTTP methods with 405_METHOD_NOT_SUPPORTED"""
-    message = str(error)
-    app.logger.warning(message)
-    return (
-        jsonify(
-            status=status.HTTP_405_METHOD_NOT_ALLOWED,
-            error="Method not Allowed",
-            message=message,
-        ),
-        status.HTTP_405_METHOD_NOT_ALLOWED,
-    )
-
-
 @app.errorhandler(status.HTTP_409_CONFLICT)
-def resource_conflict(error):
-    """Handles resource conflicts with HTTP_409_CONFLICT"""
-    message = str(error)
-    app.logger.warning(message)
+def conflict(error):
+    """Handling Conflict errors"""
+    app.logger.warning(str(error))
     return (
         jsonify(
             status=status.HTTP_409_CONFLICT,
             error="Conflict",
-            message=message,
+            message=str(error),
         ),
         status.HTTP_409_CONFLICT,
     )
 
 
-@app.errorhandler(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
-def mediatype_not_supported(error):
-    """Handles unsupported media requests with 415_UNSUPPORTED_MEDIA_TYPE"""
-    message = str(error)
-    app.logger.warning(message)
-    return (
-        jsonify(
-            status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-            error="Unsupported media type",
-            message=message,
-        ),
-        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-    )
-
-
 @app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
 def internal_server_error(error):
-    """Handles unexpected server error with 500_SERVER_ERROR"""
-    message = str(error)
-    app.logger.error(message)
+    """Handling Internal Server Errors"""
+    app.logger.error(str(error))
     return (
         jsonify(
             status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             error="Internal Server Error",
-            message=message,
+            message=str(error),
         ),
         status.HTTP_500_INTERNAL_SERVER_ERROR,
     )
